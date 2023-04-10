dd36a6818a214e659365d7bb69e24b39a97aebe6
bbohling22 <bbohling22@gmaol.com>
2023-04-10 14:22:56 -0700

    Added two js files to temporarily simulate a database and updated styiling on navbar and added pages file for shop, cart, login, and signup

---
+++ client/src/pages/Signup/index.js
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { USER } from "../../users";
+
+// export const Signup = () => {
+//     return (
+    
+
+//         <div className="signup">
+//             <div className="shopName">
+//                 <h1>Only Tomiea</h1>
+//             </div>
+//             <div className="signupInput">
+//                 {/* API call to data base of users. USERS is only for show */}
+//                 {USER.map((user) => (
+//                     <User data={user} />
+//                 ))}
+//             </div>
+//         </div> 
+    
+//     )
+// }
