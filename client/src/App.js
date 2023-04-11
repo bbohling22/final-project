d769583835cfca77fb1bdb5823cb8337784bbf73
Wixom78666 <jswix4@gmail.com>
2023-04-10 19:33:30 -0500

    trying

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import './index.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/navbar";
-import { Shop } from "./pages/Shop";
 import { Login } from "./pages/Login";
-import { Cart } from "./pages/Cart";
 import { Signup } from "./pages/Signup"
 
 
