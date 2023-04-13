bb0bc995b7eb7bde917dac47ca975ff9f4a58d58
Leslie Garcia <leslie.garcia2433@gmail.com>
2023-04-13 18:19:08 -0500

    Created team about page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup"
 import Home from "./pages/Home/Home"
 import Create from "./pages/Create/Create"
+import Team from "./pages/Team/Team"
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -29,6 +30,7 @@ function App() {
             <Route path="/Login" element={<Login />} />
             <Route path="/Signup" element={<Signup />} />
             <Route path="/Create" element={<Create />} />
+            <Route path="/about/team" element={<Team />} />
             
           </Routes>
           <Footer/>
