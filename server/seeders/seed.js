6b2905143ccf838788739dec058fa147b1d05a38
Cheung Yee Cheng <johnny.cheng.sf@gmail.com>
2023-04-13 17:27:31 -0700

    $1

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,41 @@
 const db = require('../config/connection');
 
-const { User } = require('../models');
+const { User, Post } = require('../models');
 const userSeeds = require('./userSeeds.json');
-// const imageSeeds = require('./imageSeeds.json');
+const postSeeds = require('./postSeeds.json');
 
 db.once('open', async () => {
   try {
+
+    await Post.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
-    
+    for (let i = 0; i < postSeeds.length; i++) {
+      const { _id, postAuthor } = await Post.create(postSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { email: postAuthor },
+        {
+          $addToSet: {
+            posts: _id,
+          },
+        }
+      );
+    }
+/*
+    await User.deleteMany({});
+    await Post.deleteMany({});
 
+    const createdUsers = await User.create(userSeeds);
+
+    const createdPosts = await Post.create(
+        posts.map(post = {
+          
+
+        }
+    );
+*/
   } catch (err) {
     console.error(err);
     process.exit(1);
