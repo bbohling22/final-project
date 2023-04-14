2e2010dadec026a02c15c61a56e439657a4ef508
Wixom78666 <jswix4@gmail.com>
2023-04-13 19:38:45 -0500

    Home page worked on

---
+++ client/src/components/AboutTeam/index.js
@@ -0,0 +1,125 @@
+const AboutTeam = () => {
+  return (
+    <div>
+      <div class="container hero is-fullheight has-background-signup is-fullhd  is-max-desktop">
+        <div class="columns is-centered ">
+          <div class="column is-half">
+            <div class="content mx-6 my-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://www.meme-arsenal.com/memes/803738ce945be57d5e4e3e90f54dd5f8.jpg"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                  
+                    <div class="media-content">
+                      <p class="title is-4">Bryan</p>
+                      <p class="subtitle is-6"><a href="https://github.com/bbohling22">@bbohling22</a></p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    About you goes here
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column is-half">
+            <div class="content mx-6 my-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://mountainproject.com/assets/photos/climb/108004690_medium_1494256754.jpg?cache=1389917767"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    
+                    <div class="media-content">
+                      <p class="title is-4">Jacob</p>
+                      <p class="subtitle is-6"><a href="https://github.com/Wixom78666">@Wixom78666</a></p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    About you goes here
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="columns is-centered ">
+          <div class="column is-half">
+            <div class="content mx-6 my-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://miro.medium.com/v2/0*ZjYSm_q36J4KChdn"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    
+                    <div class="media-content">
+                      <p class="title is-4">Johnny</p>
+                      <p class="subtitle is-6"><a href="https://github.com/johnnychengsf">@johnnychengsf</a></p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    About you goes here
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column is-half">
+            <div class="content mx-6 my-6">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img
+                      src="https://www.meme-arsenal.com/memes/cde035f9a785352d8bae7a4d561bf060.jpg"
+                      alt="Placeholder image"
+                    />
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    
+                    <div class="media-content">
+                      <p class="title is-4">Leslie</p>
+                      <p class="subtitle is-6"><a href="https://github.com/lesliefg">@lesliefg</a></p>
+                    </div>
+                  </div>
+
+                  <div class="content">
+                    About you goes here
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutTeam;
